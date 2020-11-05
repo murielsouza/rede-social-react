9d02ea3626184c4f243ee92532b669c7078a63bd
Muriel S <muriel-souza@live.com>
2020-11-05 15:59:16 -0300

    versão para rede social

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return(
+      <div>
+          <header className="app-header">
+              <div className="app-container">
+                  <aside className="app-menu">
+                      ...
+                  </aside>
+                  <div className="app-content">
+                      ...
+                  </div>
+
+                  <footer className="app-footer">
+                      ...
+                  </footer>
+              </div>
+          </header>
+      </div>
+  )
 }
-
-export default App;
